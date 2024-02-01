6b590e951702d34420db34e6ef2a06843b71a6a4
Alex A <alex.andsten@gmail.com>
2024-02-01 23:36:51 +0100

    start

---
+++ src/home/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'center', width:'100%', marginTop: '25px'}}>
+        <Typography sx={{ fontSize: '18px', fontWeight: 'semiBold'}} >
+            Welcome to the page of an upcoming...
+        </Typography>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', width:'100%', marginTop: '25px'}}>
+        <Typography sx={{ fontSize: '18px', fontWeight: 'semiBold'}} >
+            Welcome to the page of an upcoming...
+        </Typography>
+      </Box>
+    </>
+  )
+}
